b14c243d8ee81950963f50f1433a0ad7b245a170
Giulianno Zanetti <giulianno.zanetti@grupomultiplica.com.br>
2022-03-21 15:57:01 -0300

    Componente de Carrosel

--- src/components/CarrouselComponent/index.tsx
+++ src/components/CarrouselComponent/index.tsx
@@ -1,4 +1,13 @@
-import { ContainerCarrousel } from "./styles"
+import { ContainerCarrousel, ContainerSlide,ImagemSlide,TextoSlide,TextoNome } from "./styles"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination } from 'swiper';
+import Image from 'next/image'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import Typography from '@mui/material/Typography'
+import { Box } from "@mui/material";
 
 
 
@@ -7,10 +16,58 @@ import { ContainerCarrousel } from "./styles"
 const CarrouselComponent = () =>{
     return (
         <>
-            <ContainerCarrousel>
-                aaa
-            </ContainerCarrousel>
-        
+           <ContainerCarrousel>
+          <Swiper
+         style={{
+           "--swiper-navigation-color": "#fff",
+          "--swiper-pagination-color": "#fff",
+         }} 
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+            <ContainerSlide>
+                 <Box display={'grid'}>
+                  <Image src="/assets/images/user.png" alt="logo" width="128" height="128" className="img-teste" />
+                  <TextoNome variant="h6" >Matheus Silva</TextoNome>
+                 </Box>
+                      
+                  <ImagemSlide>
+                    <TextoSlide variant="h4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, optio dolorem? Obcaecati in debitis doloremque perspiciatis modi dignissimos ullam accusantium veritatis reprehenderit vel laboriosam consequuntur voluptatum, asperiores voluptatibus molestias cum.
+                    </TextoSlide>
+                    
+                  </ImagemSlide>
+                 
+            </ContainerSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+            <ContainerSlide>
+                 <Box display={'grid'}>
+                  <Image src="/assets/images/fundo.jpg" alt="logo" width="128" height="128" className="img-teste" />
+                  <TextoNome variant="h6" >José Pereira</TextoNome>
+                 </Box>
+                      
+                  <ImagemSlide>
+                    <TextoSlide variant="h4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, optio dolorem? Obcaecati in debitis doloremque perspiciatis modi dignissimos ullam accusantium veritatis reprehenderit vel laboriosam consequuntur voluptatum, asperiores voluptatibus molestias cum.
+                    </TextoSlide>
+                    
+                  </ImagemSlide>
+            </ContainerSlide>
+        </SwiperSlide>
+      </Swiper>
+        </ContainerCarrousel>
         </>
     )
 
--- src/components/CarrouselComponent/styles.ts
+++ src/components/CarrouselComponent/styles.ts
@@ -1,12 +1,42 @@
-import { Box, styled } from "@mui/material";
+import { Box, styled, Typography } from "@mui/material";
 
 
 
 
 export const ContainerCarrousel = styled(Box)`
     width: 100%;
-    height: 250px;
+    height: 550px;
     border-radius: 1px solid black;
 
+
+`
+export const ContainerSlide = styled(Box)`
+    width: 70%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const ImagemSlide = styled(Box)`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+
+`
+export const TextoSlide = styled(Typography)`
+   font-family: 'Quicksand';
+   font-size: 20px;
+    
+
+`
+export const TextoNome = styled(Typography)`
+   font-family: 'Quicksand';
+   font-size: 20px;
+   width: 130px;
+    
 
 `
