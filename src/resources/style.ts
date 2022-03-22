6441e91e74fdc9d127da7047c79fdf563455d209
Giulianno Zanetti <giulianno.zanetti@grupomultiplica.com.br>
2022-03-22 14:32:33 -0300

    Arrumando layout

--- src/resources/style.ts
+++ src/resources/style.ts
@@ -2,7 +2,7 @@ import { Box, styled, Typography } from "@mui/material";
 
 
 export const BoxConteudo = styled(Box)`
-    padding: 30px  250px;
+    padding: 20px  420px;
     height: 100vh;
     
 `
