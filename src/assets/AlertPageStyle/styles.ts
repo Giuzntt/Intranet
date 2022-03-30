dc161c6430610822a0548c4fabfcad71ed0943a8
Giulianno Zanetti <giulianno.zanetti@grupomultiplica.com.br>
2022-03-30 14:43:35 -0300

    task Chamados

---
+++ src/assets/AlertPageStyle/styles.ts
@@ -0,0 +1,13 @@
+//criar uma Box Container coom Material Design Styled
+
+import { Search } from "@mui/icons-material";
+import { Box, Button, styled, TextField } from "@mui/material";
+
+
+export const BoxAlert = styled(Box)`
+  padding: 120px 360px;
+  height: 100vh;
+  border: 1px solid #e0e0e0;
+`;
+
+
